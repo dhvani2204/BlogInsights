541e1a4724330809bbe5996215fd074896c5a0be
Dhvani Parekh <dhvani2204@gmail.com>
2024-04-21 20:39:52 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import userRoutes from './routes/user.route.js'
+
 
 mongoose
 .connect('mongodb://127.0.0.1:27017/BlogInsights')
@@ -15,3 +17,4 @@ const app=express();
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!')
 });
+app.use('/api/user', userRoutes);
