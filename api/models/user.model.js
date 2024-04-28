c1f5e6ba172cad917eadc51f2647342596a7f715
Dhvani Parekh <dhvani2204@gmail.com>
2024-04-28 12:27:54 +0530

    add admin functionality to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -20,7 +20,10 @@ profilePicture:{
     type:String,
     default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"     
 },
-
+isAdmin:{
+    type:Boolean,
+    default:false,
+},
 },{timestamps:true}//need to store time of creation and update
 );
 
